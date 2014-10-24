fe2700245a3118b3ef7185ae6ea2715a31f4d20b
ZirconCode <gruenings@gmail.com>
2014-10-24 23:54:20 +0900

    added amazing menu

--- src/com/zirconcode/grassgrowingsimulator/MainActivity.java
+++ src/com/zirconcode/grassgrowingsimulator/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.activity_main);
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
